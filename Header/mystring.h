efd3364a378f9a01592682de5f8c4bdea0aa06c2
double Z <doubleZ0108@163.com>
2020-01-22 12:31:52 +0800

    add test program

---
+++ Header/mystring.h
@@ -0,0 +1,12 @@
+//
+//  mystring.h
+//  MyString
+//  
+//  Created by double Z on 2020/01/22 .
+//  Copyright © 2019 double Z. All rights reserved.
+//
+
+#ifndef MYSTRING_MYSTRING_H
+#define MYSTRING_MYSTRING_H
+
+#endif //MYSTRING_MYSTRING_H
