2a1dbef45fc737c18cddc626714f69c558239cc2
double Z <doubleZ0108@163.com>
2020-01-22 13:31:35 +0800

    add basic function

--- Header/mystring.h
+++ Header/mystring.h
@@ -9,4 +9,97 @@
 #ifndef MYSTRING_MYSTRING_H
 #define MYSTRING_MYSTRING_H
 
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+class MyString{
+
+    friend ostream& operator<<(ostream& os, const MyString& mystr);
+
+private:
+    char* str = NULL;
+    int length = 0;
+
+public:
+    /* Constructor */
+    MyString() = default;
+
+    MyString(const char* source_str){
+        this->length = strlen(source_str);
+        int temp_size = sizeof(char) * this->length;
+        this->str = (char *)malloc(temp_size);
+        memcpy(this->str, source_str, temp_size);
+    }
+
+    MyString(const MyString& source_mystr){
+        this->length = source_mystr.size();
+        int temp_size = sizeof(char) * this->length;
+        this->str = (char *)malloc(temp_size);
+        memcpy(this->str, source_mystr.getStr(), temp_size);
+    }
+
+    /* Destructor */
+    ~MyString(){
+        if(str){
+            free(this->str);
+            this->str = NULL;
+        }
+    }
+
+    /* Getter */
+    char* getStr() const{
+        return this->str;
+    }
+
+    int size() const{
+        return this->length;
+    }
+
+    /* Operator */
+    char& operator[](int index){
+        return *(str + index);
+    }
+
+    const char operator[](int index) const{
+        return *(str + index);
+    }
+
+    /* Append */
+    void append(const char* appendix_str){
+        if(this->str){
+            char* buf_str = (char *)malloc(sizeof(char)*this->length);
+            memcpy(buf_str, this->str, sizeof(char)*this->length);
+
+            int origin_len = this->length;
+            int fresh_len = strlen(appendix_str);
+            this->length += fresh_len;
+
+            free(this->str);
+            this->str = NULL;
+            this->str = (char *)malloc(sizeof(char)*this->length);
+            memcpy(this->str, buf_str, sizeof(char)*origin_len);
+            memcpy(this->str+origin_len, appendix_str, sizeof(char)*fresh_len);
+        }else{
+            this->length = strlen(appendix_str);
+            int temp_size = sizeof(char) * this->length;
+            this->str = (char *)malloc(temp_size);
+            this->str = (char *)memcpy(this->str, appendix_str, temp_size);
+        }
+    }
+
+    void append(const MyString& appendix_mystr){
+        
+    }
+};
+
+ostream& operator<<(ostream& os, const MyString& mystr){
+    int length = mystr.size();
+    for(int i=0;i<length;++i){
+        os << mystr[i];
+    }
+    return os;
+}
+
 #endif //MYSTRING_MYSTRING_H
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "../Header/mystring.h"
+using namespace std;
+
+void testBasicFunction(){
+    MyString mystr;
+    MyString mystr2("a_string");
+    MyString mystr3(mystr2);
+
+    cout << mystr.size() << endl;
+    cout << mystr2.size() << endl;
+
+    cout << mystr3 << endl;
+
+    mystr3[1] = ' ';
+    cout << mystr3 << endl;
+
+    mystr3[2] = 'b';
+    cout << mystr3 << endl;
+    cout << mystr2[1] << endl;
+
+    mystr.append("foobar");
+    cout << mystr.size() << endl;
+
+    mystr2.append("_boxboz");
+    cout << mystr2 << endl;
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    testBasicFunction();
+
     return 0;
 }
